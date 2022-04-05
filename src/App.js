8c9aae14f521e6d2b7b3bc9cfd88e12f419b14ed
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-04-05 17:39:17 -0500

    set up landing page and a few components for login and register page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
+import { Link, Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <Link 
+        to='/'
+      ><h1>Home Page</h1></Link>
+      <Link
+        to='/login'
+      >Login</Link>
+      <Outlet/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import {render} from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './components/Login';
 
 
-render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+
+
+root.render(
   <Router>
     <Routes>
       <Route path='/' element={<App />}>
+        <Route path='login' element={<Login />} />
       </Route>
     </Routes>
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
